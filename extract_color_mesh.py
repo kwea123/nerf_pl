9dc0a79bffd512cb631cf0a7221854747031c441
kwea123 <kwea123@gmail.com>
2020-05-20 23:16:57 +0900

    add vertex normal method

--- extract_color_mesh.py
+++ extract_color_mesh.py
@@ -52,11 +52,19 @@ def get_opts():
                         help='''threshold to consider a vertex is occluded.
                                 larger=fewer occluded pixels''')
 
+    #### method using vertex normals ####
+    parser.add_argument('--use_vertex_normal', action="store_true",
+                        help='use vertex normals to compute color')
+    parser.add_argument('--N_importance', type=int, default=64,
+                        help='number of fine samples to infer the acculmulated opacity')
+    parser.add_argument('--near_t', type=float, default=1.0,
+                        help='the near bound factor to start the ray')
+
     return parser.parse_args()
 
 
 @torch.no_grad()
-def f(models, embeddings, rays, N_samples, chunk, white_back):
+def f(models, embeddings, rays, N_samples, N_importance, chunk, white_back):
     """Do batched inference on rays using chunk."""
     B = rays.shape[0]
     results = defaultdict(list)
@@ -69,7 +77,7 @@ def f(models, embeddings, rays, N_samples, chunk, white_back):
                         False,
                         0,
                         0,
-                        0,
+                        N_importance,
                         chunk,
                         white_back,
                         test_time=True)
@@ -137,6 +145,12 @@ if __name__ == "__main__":
     ##### Until mesh extraction here, it is the same as the original repo. ######
 
     vertices_ = (vertices/N).astype(np.float32)
+    ## invert x and y coordinates (WHY? maybe because of the marching cubes algo)
+    x_ = (ymax-ymin) * vertices_[:, 1] + ymin
+    y_ = (xmax-xmin) * vertices_[:, 0] + xmin
+    vertices_[:, 0] = x_
+    vertices_[:, 1] = y_
+    vertices_[:, 2] = (zmax-zmin) * vertices_[:, 2] + zmin
     vertices_.dtype = [('x', 'f4'), ('y', 'f4'), ('z', 'f4')]
 
     face = np.empty(len(triangles), dtype=[('vertex_indices', 'i4', (3,))])
@@ -167,87 +181,103 @@ if __name__ == "__main__":
 
     # Step 1. transform vertices into world coordinate
     N_vertices = len(vertices_)
-
-    ## invert x and y coordinates (WHY? maybe because of the marching cubes algo)
-    x_ = (ymax-ymin) * vertices_[:, 1] + ymin
-    y_ = (xmax-xmin) * vertices_[:, 0] + xmin
-    vertices_[:, 0] = x_
-    vertices_[:, 1] = y_
-    vertices_[:, 2] = (zmax-zmin) * vertices_[:, 2] + zmin
     vertices_homo = np.concatenate([vertices_, np.ones((N_vertices, 1))], 1) # (N, 4)
 
-    ## buffers to store the final averaged color
-    non_occluded_sum = np.zeros((N_vertices, 1))
-    v_color_sum = np.zeros((N_vertices, 3))
+    if args.use_vertex_normal: ## use normal vector method as suggested by the author.
+                               ## see https://github.com/bmild/nerf/issues/44
+        mesh.compute_vertex_normals()
+        rays_d = torch.FloatTensor(np.asarray(mesh.vertex_normals))
+        near = dataset.bounds.min() * torch.ones_like(rays_d[:, :1])
+        far = dataset.bounds.max() * torch.ones_like(rays_d[:, :1])
+        rays_o = torch.FloatTensor(vertices_) - rays_d * near * args.near_t
 
-    # Step 2. project the vertices onto each training image to infer the color
-    print('Fusing colors ...')
-    for idx in tqdm(range(len(dataset.image_paths))):
-        ## read image of this pose
-        image = cv2.imread(dataset.image_paths[idx])[:,:,::-1]
-        image = cv2.resize(image, tuple(args.img_wh))
+        nerf_coarse = NeRF()
+        load_ckpt(nerf_coarse, args.ckpt_path, model_name='nerf_coarse')
+        nerf_coarse.cuda().eval()
 
-        ## read the camera to world relative pose
-        P_c2w = np.concatenate([dataset.poses[idx], np.array([0, 0, 0, 1]).reshape(1, 4)], 0)
-        P_w2c = np.linalg.inv(P_c2w)[:3] # (3, 4)
-        ## project vertices from world coordinate to camera coordinate
-        vertices_cam = (P_w2c @ vertices_homo.T) # (3, N) in "right up back"
-        vertices_cam[1:] *= -1 # (3, N) in "right down forward"
-        ## project vertices from camera coordinate to pixel coordinate
-        vertices_image = (K @ vertices_cam).T # (N, 3)
-        depth = vertices_image[:, -1:]+1e-5 # the depth of the vertices, used as far plane
-        vertices_image = vertices_image[:, :2]/depth
-        vertices_image = vertices_image.astype(np.float32)
-        vertices_image[:, 0] = np.clip(vertices_image[:, 0], 0, W-1)
-        vertices_image[:, 1] = np.clip(vertices_image[:, 1], 0, H-1)
-
-        ## compute the color on these projected pixel coordinates
-        ## using bilinear interpolation.
-        ## NOTE: opencv's implementation has a size limit of 32768 pixels per side,
-        ## so we split the input into chunks.
-        colors = []
-        remap_chunk = int(3e4)
-        for i in range(0, N_vertices, remap_chunk):
-            colors += [cv2.remap(image, 
-                                 vertices_image[i:i+remap_chunk, 0],
-                                 vertices_image[i:i+remap_chunk, 1],
-                                 interpolation=cv2.INTER_LINEAR)[:, 0]]
-        colors = np.vstack(colors) # (N_vertices, 3)
-        
-        ## predict occlusion of each vertex
-        ## we leverage the concept of NeRF by constructing rays coming out from the camera
-        ## and hitting each vertex; by computing the accumulated opacity along this path,
-        ## we can know if the vertex is occluded or not.
-        ## for vertices that appear to be occluded from every input view, we make the
-        ## assumption that its color is the same as its neighbors that are facing our side.
-        ## (think of a surface with one side facing us: we assume the other side has the same color)
-
-        ## ray's origin is camera origin
-        rays_o = torch.FloatTensor(dataset.poses[idx][:, -1]).expand(N_vertices, 3)
-        ## ray's direction is the vector pointing from camera origin to the vertices
-        rays_d = torch.FloatTensor(vertices_) - rays_o # (N_vertices, 3)
-        rays_d = rays_d / torch.norm(rays_d, dim=-1, keepdim=True)
-        near = dataset.bounds.min() * torch.ones_like(rays_o[:, :1])
-        ## the far plane is the depth of the vertices, since what we want is the accumulated
-        ## opacity along the path from camera origin to the vertices
-        far = torch.FloatTensor(depth) * torch.ones_like(rays_o[:, :1])
-        results = f([nerf_fine], embeddings,
+        results = f([nerf_coarse, nerf_fine], embeddings,
                     torch.cat([rays_o, rays_d, near, far], 1).cuda(),
                     args.N_samples,
+                    args.N_importance,
                     args.chunk,
                     dataset.white_back)
-        opacity = results['opacity_coarse'].cpu().numpy()[:, np.newaxis] # (N_vertices, 1)
 
-        non_occluded = np.ones_like(non_occluded_sum) * 0.1/depth # weight by inverse depth
-                                                                # near=more confident in color
-        non_occluded += opacity < args.occ_threshold
-        
-        v_color_sum += colors * non_occluded
-        non_occluded_sum += non_occluded
+    else: ## use my color average method. see README_mesh.md
+        ## buffers to store the final averaged color
+        non_occluded_sum = np.zeros((N_vertices, 1))
+        v_color_sum = np.zeros((N_vertices, 3))
+
+        # Step 2. project the vertices onto each training image to infer the color
+        print('Fusing colors ...')
+        for idx in tqdm(range(len(dataset.image_paths))):
+            ## read image of this pose
+            image = cv2.imread(dataset.image_paths[idx])[:,:,::-1]
+            image = cv2.resize(image, tuple(args.img_wh))
+
+            ## read the camera to world relative pose
+            P_c2w = np.concatenate([dataset.poses[idx], np.array([0, 0, 0, 1]).reshape(1, 4)], 0)
+            P_w2c = np.linalg.inv(P_c2w)[:3] # (3, 4)
+            ## project vertices from world coordinate to camera coordinate
+            vertices_cam = (P_w2c @ vertices_homo.T) # (3, N) in "right up back"
+            vertices_cam[1:] *= -1 # (3, N) in "right down forward"
+            ## project vertices from camera coordinate to pixel coordinate
+            vertices_image = (K @ vertices_cam).T # (N, 3)
+            depth = vertices_image[:, -1:]+1e-5 # the depth of the vertices, used as far plane
+            vertices_image = vertices_image[:, :2]/depth
+            vertices_image = vertices_image.astype(np.float32)
+            vertices_image[:, 0] = np.clip(vertices_image[:, 0], 0, W-1)
+            vertices_image[:, 1] = np.clip(vertices_image[:, 1], 0, H-1)
+
+            ## compute the color on these projected pixel coordinates
+            ## using bilinear interpolation.
+            ## NOTE: opencv's implementation has a size limit of 32768 pixels per side,
+            ## so we split the input into chunks.
+            colors = []
+            remap_chunk = int(3e4)
+            for i in range(0, N_vertices, remap_chunk):
+                colors += [cv2.remap(image, 
+                                    vertices_image[i:i+remap_chunk, 0],
+                                    vertices_image[i:i+remap_chunk, 1],
+                                    interpolation=cv2.INTER_LINEAR)[:, 0]]
+            colors = np.vstack(colors) # (N_vertices, 3)
+            
+            ## predict occlusion of each vertex
+            ## we leverage the concept of NeRF by constructing rays coming out from the camera
+            ## and hitting each vertex; by computing the accumulated opacity along this path,
+            ## we can know if the vertex is occluded or not.
+            ## for vertices that appear to be occluded from every input view, we make the
+            ## assumption that its color is the same as its neighbors that are facing our side.
+            ## (think of a surface with one side facing us: we assume the other side has the same color)
+
+            ## ray's origin is camera origin
+            rays_o = torch.FloatTensor(dataset.poses[idx][:, -1]).expand(N_vertices, 3)
+            ## ray's direction is the vector pointing from camera origin to the vertices
+            rays_d = torch.FloatTensor(vertices_) - rays_o # (N_vertices, 3)
+            rays_d = rays_d / torch.norm(rays_d, dim=-1, keepdim=True)
+            near = dataset.bounds.min() * torch.ones_like(rays_o[:, :1])
+            ## the far plane is the depth of the vertices, since what we want is the accumulated
+            ## opacity along the path from camera origin to the vertices
+            far = torch.FloatTensor(depth) * torch.ones_like(rays_o[:, :1])
+            results = f([nerf_fine], embeddings,
+                        torch.cat([rays_o, rays_d, near, far], 1).cuda(),
+                        args.N_samples,
+                        0,
+                        args.chunk,
+                        dataset.white_back)
+            opacity = results['opacity_coarse'].cpu().numpy()[:, np.newaxis] # (N_vertices, 1)
+
+            non_occluded = np.ones_like(non_occluded_sum) * 0.1/depth # weight by inverse depth
+                                                                    # near=more confident in color
+            non_occluded += opacity < args.occ_threshold
+            
+            v_color_sum += colors * non_occluded
+            non_occluded_sum += non_occluded
 
     # Step 3. combine the output and write to file
-    ## the combined color is the average color among all views
-    v_colors = v_color_sum/non_occluded_sum
+    if args.use_vertex_normal:
+        v_colors = results['rgb_fine'].cpu().numpy() * 255.0
+    else: ## the combined color is the average color among all views
+        v_colors = v_color_sum/non_occluded_sum
     v_colors = v_colors.astype(np.uint8)
     v_colors.dtype = [('red', 'u1'), ('green', 'u1'), ('blue', 'u1')]
     vertices_.dtype = [('x', 'f4'), ('y', 'f4'), ('z', 'f4')]
